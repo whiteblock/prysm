78ea7feb9b701b8cb74d6bb7f74cadf6bdef26c2
renaynay <renenayman11@gmail.com>
2019-06-29 17:37:29 -0400

    b

--- shared/p2p/hobbits/server.go
+++ shared/p2p/hobbits/server.go
@@ -29,3 +29,24 @@ func (h *HobbitsNode) Send(msg HobbitsMessage, peer string, conn net.Conn) error
 
 	return nil
 }
+
+func (h *HobbitsNode) Broadcast(msg HobbitsMessage) error {
+	for _, peer := range h.staticPeers {
+		err := h.Send(msg, peer, ) //How to pass in a conn?)
+		if err != nil {
+			return errors.Wrap(err, "error broadcasting: ")
+		}
+	}
+
+	return nil
+}
+
+func (h *HobbitsNode) Listen() (net.Conn, error) {
+	server := tcp.NewServer(h.host, h.port)
+	ch := make(chan encoding.Message)
+
+	go server.Listen(func(_ net.Conn, message encoding.Message){
+		ch <- message
+	})
+
+}
