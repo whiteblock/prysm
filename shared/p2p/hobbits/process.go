8a158403cb0df32a1daeed5841a18c6144c6338d
renaynay <renenayman11@gmail.com>
2019-07-01 09:24:28 -0400

    creating tests

--- shared/p2p/hobbits/process.go
+++ shared/p2p/hobbits/process.go
@@ -1,1 +1,28 @@
 package hobbits
+
+import "github.com/pkg/errors"
+
+func (h *HobbitsNode) processHobbitsMessage(message HobbitsMessage) error {
+	switch message.Protocol {
+	case "RPC":
+		err := h.processRPC(message)
+		if err != nil {
+			return errors.Wrap(err, "error processing an RPC hobbits message")
+		}
+	case "GOSSIP":
+		err := h.processGossip(message)
+		if err != nil {
+			return errors.Wrap(err, "error processing a GOSSIP hobbits message")
+		}
+	}
+
+	return nil
+}
+
+func (h *HobbitsNode) processRPC(message HobbitsMessage) error {
+
+}
+
+func (h *HobbitsNode) processGossip(message HobbitsMessage) error {
+
+}
---
+++ shared/p2p/hobbits/process_test.go
@@ -0,0 +1,7 @@
+package hobbits
+
+import (
+	"testing"
+)
+
+
